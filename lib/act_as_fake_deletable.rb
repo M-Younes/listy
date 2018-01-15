ed26d36d03f54438c5957cb8eade5505925d219e
Younes <m.younes87@gmail.com>
2018-01-15 15:29:40 +0800

    act as face deletable reusable

--- lib/act_as_fake_deletable.rb
+++ lib/act_as_fake_deletable.rb
@@ -2,15 +2,11 @@ module ActAsFakeDeletable
 
   # override the model actions
   def delete
-    self.update_attributes(deleted_at: DateTime.current)
-    self.list_items.each{|list_item| list_item.delete } if self.class.name == "List"
-    self.tag_items.update_all(deleted_at: DateTime.current) if self.class.name == "ListItem"
+    self.delete_attributes
   end 
 
   def undelete # to "restore" the file
-    self.update_attributes(deleted_at: nil)
-    self.list_items.each{|list_item| list_item.undelete } if self.class.name == "List"
-    self.tag_items.update_all(deleted_at: nil) if self.class.name == "ListItem"
+    self.undelete_attributes
   end
   # define new scopes
   def self.included(base)
@@ -19,5 +15,29 @@ module ActAsFakeDeletable
       scope :destroyed, -> {where("(deleted_at IS NOT NULL)")}
     end
   end
+
+  def delete_attributes
+    return self.deleted_at unless defined? (self.deleted_at)
+    self.update_attributes(deleted_at: DateTime.current)
+    self.delete_associated_attributes
+  end
+
+  def delete_associated_attributes
+    self.class.reflections.select{|association_name, reflection| reflection.macro == :has_many && reflection.options[:dependent] == :destroy}.each do |associations|
+      self.send(associations.first).each{|association| association.delete }
+    end
+  end
+
+  def undelete_attributes
+    return self.deleted_at unless defined? (self.deleted_at)
+    self.update_attributes(deleted_at: nil)
+    self.undelete_associated_attributes
+  end
+
+  def undelete_associated_attributes
+    self.class.reflections.select{|association_name, reflection| reflection.macro == :has_many && reflection.options[:dependent] == :destroy}.each do |associations|
+      self.send(associations.first).each{|association| association.undelete }
+    end
+  end  
 
 end
